4768af9b1d4b9904c43f33d9fa26d45a8272b48f
Dai Maou <daimaou92@outlook.com>
2020-05-23 11:18:16 +0530

    added vol and media key binds

--- config.h
+++ config.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -59,6 +60,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+10%", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-10%", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
+static const char *muteMic[] = { "/usr/bin/pactl", "set-source-mute", "@DEFAULT_SOURCE@", "toggle", NULL };
+static const char *playpause[] = { "/usr/bin/mpc", "toggle",  NULL };
+static const char *playnext[] = { "/usr/bin/mpc", "next", NULL };
+static const char *playprevious[] = { "/usr/bin/mpc", "prev", NULL };
+// static const char *brightnessup[] = { "/usr/bin/xbacklight", "-inc", "10", NULL };
+// static const char *brightnessdown[] = { "/usr/bin/xbacklight", "-dec", "10", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -99,6 +109,13 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,                            XF86XK_AudioLowerVolume,  spawn,    {.v = downvol } },
+	{ 0,                            XF86XK_AudioMute,         spawn,    {.v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume,  spawn,    {.v = upvol   } },
+	{ 0,                            XF86XK_AudioPlay,         spawn,    {.v = playpause} },
+	{ 0,                            XF86XK_AudioNext,         spawn,    {.v = playnext} },
+	{ 0,                            XF86XK_AudioPrev,         spawn,    {.v = playprevious} },
+	{ 0,                            XF86XK_AudioMicMute,      spawn,    {.v = muteMic} },
 };
 
 /* button definitions */
